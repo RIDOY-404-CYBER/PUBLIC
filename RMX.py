2504d25531083966cd51f78ef31cbc9da490f1d3
PICCI-RIDOY <107355397+RIDOY-404-CYBER@users.noreply.github.com>
2024-01-27 23:16:59 +0600

    Update RMX.py

--- RMX.py
+++ RMX.py
@@ -1,9 +1,32 @@
-import os,platform
-os.system('clear')
-print('[•] Checking Updates...')
-os.system('git pull')
-bit = platform.architecture()[0]
-if bit=='64bit':
-    print('\033[1;32m[•] Congrats! Your Device Support This Tools')
-    os.system('xdg-open https://facebook.com/groups/770617227293870/')
-    import PUBLIC13
+#!usr/bin/env python
+# -*- coding:utf-8
+#CODE BY RMX-XD
+"""
+CREATED By RMX, 27 January 2024
+@AUTHOR: ADRIAN WAYNE RIDOY 
+"""
+##MAIN##
+class RMX_XD:
+   def __init__(self):
+       self.update()
+       self.main_()
+ 
+   def update(self):
+       import os
+       os.system('git pull')
+ 
+   def main_(self):
+       import os,platform
+       bit = platform.architecture()[0]
+      if bit=='64bit':
+        print('\033[1;32m[•] Congrats! Your Device Support This Tools')
+        #os.system('xdg-open https://facebook.com/groups/770617227293870/')
+        import PUBLICX
+      if rmx=='32bit':
+        print('\033[1;32m[•] Congrats! Your Device Support This Tools')
+        #os.system('xdg-open https://facebook.com/groups/770617227293870/')
+        import PUBLICX2
+      else:print('\033[1;32m[•] Contract Admin For Help');exit()
+  
+RMX_XD()
+##
